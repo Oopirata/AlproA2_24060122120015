f8596ccb4e50bc9294228e34502ca66865b91829
Oopirata <126538833+Oopirata@users.noreply.github.com>
2023-03-10 20:21:09 +0700

    Add files via upload

---
+++ Praktikum_1/jarakGLBB.c
@@ -0,0 +1,27 @@
+/* Nama File    		: jarakGLBB.c */
+/* Deskripsi    		: Menghitung dan menampilkan jarak S di layar, S = v0 x t + 1/2 x (a x tkuadrat)*/
+/* Pembuat      		: Hanif Herofa - 24060122120015 */
+/*Tanggal Pembuatan		: 22 Februari 2022 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float v0;
+    float t;
+    float a;
+    float S;
+
+    printf("Masukkan kecepatan awal :");
+    scanf ("%f",&v0);
+    printf("Masukkan waktu :");
+    scanf ("%f",&t);
+    printf("Masukkan percepatan :");
+    scanf ("%f",&a);
+
+
+    S = v0 * t + 0.5 * (a*(t*t));
+    printf ("Hasilnya adalah : %f", S);
+    return 0;
+}
---
+++ Praktikum_1/volBolaKerct.c
@@ -0,0 +1,27 @@
+/* Nama File    		: volBolaKerct.c */
+/* Deskripsi    		: Menghitung dan menampilkan volume bola Vb di layar, Vb = 4/3 x (Phi x rpangkat3), Menghitung dan menampilkan volume kerucut Vk di layar, Vk = 1/2 x Vb */
+/* Pembuat      		: Hanif Herofa - 24060122120015 */
+/*Tanggal Pembuatan		: 26 Februari 2022 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float r;
+    float Vb;
+    float Phi = 3.14;
+    float Vk;
+
+    printf("Masukkan jari-jari :");
+    scanf ("%f",&r);
+
+
+    Vb = 1.3 * (Phi * (r*r*r));
+    printf("Volume Bola adalah : %f", Vb);
+
+    Vk = 0.5 * Vb;
+    printf("\nVolume Kerucut adalah : %f", Vk);
+
+    return 0;
+}
